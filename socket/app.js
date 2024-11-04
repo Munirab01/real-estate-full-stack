0614b8aca4439fb1caeb81b242946d97ef10ccf9
MUNIRA BHALAM <144359557+Munirab01@users.noreply.github.com>
2024-11-05 01:08:55 +0530

    Update app.js

--- socket/app.js
+++ socket/app.js
@@ -2,7 +2,7 @@ import { Server } from "socket.io";
 
 const io = new Server({
   cors: {
-    origin: "https://real-estate-full-stack-client.onrender.com/",
+    origin: "http://localhost:5173",
   },
 });
 
