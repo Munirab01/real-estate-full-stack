24f67ba249f759393b3dffcf8373dc98d9e5af88
MUNIRA BHALAM <144359557+Munirab01@users.noreply.github.com>
2024-11-05 01:18:51 +0530

    add server url to apiRequest.js

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: "https://real-estate-full-stack-95w7.onrender.com",
   withCredentials: true,
 });
 
