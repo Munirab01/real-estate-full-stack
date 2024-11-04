c4b6e68385676ee98fb407a1542d798588084e68
MUNIRA BHALAM <144359557+Munirab01@users.noreply.github.com>
2024-11-04 22:29:16 +0530

    Update apiRequest.js

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: "https://real-estate-full-stack-2.onrender.com",
   withCredentials: true,
 });
 
